268124ee11da304225b9aff5db3d49c579a0c2b6
AndresGN97 <andresguevaran@usantotomas.edu.co>
2021-10-12 00:39:05 +0000

    Arreglos

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,6 +1,11 @@
 #include "main.h"
 
+/**
+ *reset_to_98 - takes a pointer
+ *@n: integer
+ *return: 0
+ */
 void reset_to_98(int *n)
 {
-  *n = 98;
+*n = 98;
 }
--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,14 +1,15 @@
 #include "main.h"
 
 /**
- * _strlen - The lenght of a string.
- *@c: A pointer in char .
+ *_strlen - The lenght of a string.
+ *@s: A pointer in char .
+ *
  * Return: Always 0.
  */
 int _strlen(char *s)
 {
-  int a;
-  for (a = 0; *s != '\0'; a++)
-    s++;
-  return a;
+int a;
+for (a = 0; *s != '\0'; a++)
+s++;
+return (a);
 }
